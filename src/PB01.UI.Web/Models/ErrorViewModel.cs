2a6c66d228898dec526980d0ff4443540fe97681
Carlos Arjona <eduardo.arjona@hotmail.com>
2018-04-23 19:57:18 -0400

    Add project files.

---
+++ src/PB01.UI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PB01.UI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
