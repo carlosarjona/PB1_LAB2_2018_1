e0484b7f2d6ab548aa09d33364d42c558a8f1da6
Carlos Arjona <eduardo.arjona@hotmail.com>
2018-04-26 07:53:57 -0400

    fazendo o povoamento

--- src/PB01.Infrastructure/Data/DbInitializer.cs
+++ src/PB01.Infrastructure/Data/DbInitializer.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using PB01.AplicationCore.Entity;
 
 namespace PB01.Infrastructure.Data
 {
    public static class DbInitializer
     {
+        public static void Initialize(BemPatrimonialContext context)
+        {
+            if (context.BensPatrimoniais.Any())
+            {
+                return;
+            }
 
+            var bensPatrimoniais = new BemPatrimonial[]
+            {
+                new BemPatrimonial
+                {
+                    Descricao = "casa"
+                }
+            }
+            
+
+        }
+
+        
     }
 }
