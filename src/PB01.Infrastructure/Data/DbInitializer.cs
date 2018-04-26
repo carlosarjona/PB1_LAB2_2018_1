2eb6e8e2643d18263c2ce9b18ddc852aeb438612
Carlos Arjona <eduardo.arjona@hotmail.com>
2018-04-26 17:43:45 -0400

    DbInitializer

--- src/PB01.Infrastructure/Data/DbInitializer.cs
+++ src/PB01.Infrastructure/Data/DbInitializer.cs
@@ -15,15 +15,101 @@ namespace PB01.Infrastructure.Data
                 return;
             }
 
-            var bensPatrimoniais = new BemPatrimonial[]
+            var BemPatrimonial = new BemPatrimonial[]
             {
-                new BemPatrimonial
-                {
-                    Descricao = "casa"
-                }
-            }
-            
+                new BemPatrimonial {
+                    Descricao = "Fulano da Silva",
+                    DataCadastro = new DateTime(20-05-2018),
+                    NumeroTombo = "A1B2"
+                },
 
+                new BemPatrimonial {
+                    Descricao = "Beltrano da Silva",
+                    DataCadastro = new DateTime(22-02-2018),
+                    NumeroTombo = "A5C4"
+                }
+            };
+
+            context.AddRange(BemPatrimonial);
+
+            var enderecos = new Endereco[]
+            {
+                new Endereco {
+                    CEP = "78058788",
+                    Bairro = "Itiquira",
+                    Logradouro = "Rua 10",
+                    Numero = "7341",
+                    
+                },
+
+                new Endereco {
+                    CEP = "78000999",
+                    Bairro = "Dom Aquino",
+                    Logradouro = "Rua 11",
+                    Numero = "15",
+                    
+                }
+            };
+
+            context.AddRange(enderecos);
+
+            var Responsavel = new Responsavel[]
+            {
+                new Responsavel
+                {
+                   Email = "cuiaba@123.com",
+                    Nome = "jose da silva"
+                },
+
+                new Responsavel
+                {
+                    Email = "cuiaba@12345.com",
+                    Nome = "mario da silva"
+                }
+
+            };
+
+            context.AddRange(Responsavel);
+
+            var especies = new Categoria[]
+            {
+
+                new Categoria
+                {
+                    Descricao = "Predio"
+
+                },
+
+                new Categoria
+                {
+
+                    Descricao = "Condominio"
+                }
+
+
+            };
+
+            context.AddRange(typeof(Categoria));
+
+            var consultas = new Auditoria[]
+            {
+                new Auditoria
+                {
+                    Data  = new DateTime(01/01/2018),
+                    Aprovado = true
+                },
+
+                new Auditoria
+                {
+                    Data  = new DateTime(02/01/2018),
+                    Aprovado = true
+                }
+
+            };
+
+            context.AddRange(typeof(Auditoria));
+
+            context.SaveChanges();
         }
 
         
